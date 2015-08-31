bdf2c29dd6719cbeade50da626745ff94e3a01bf
Dave K <dave@paddez.com>
2015-08-31 11:35:12 +0100

    Headers

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // set the date we're counting down to
-var target_date = new Date('Feb, 13, 2016').getTime();
+var target_date = new Date(1455363000000).getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
